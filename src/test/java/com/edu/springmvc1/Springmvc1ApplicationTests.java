e45b6c2084df60e7cfcb25c4f8cd6e9a75fc8dbf
jaeheonYoo325 <yoojh0325@gmail.com>
2022-06-21 10:29:51 +0900

    2022.06.21 : Spring MVC1 init.

---
+++ src/test/java/com/edu/springmvc1/Springmvc1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.springmvc1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springmvc1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
