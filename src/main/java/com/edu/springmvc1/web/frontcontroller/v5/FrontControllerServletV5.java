e67a3350bf0aa7fa5fc804c6d596ecb22acba5fe
jaeheonYoo325 <yoojh0325@gmail.com>
2022-06-26 00:06:50 +0900

    2022.06.26 : 유연한 컨트롤러2 - v5.

--- src/main/java/com/edu/springmvc1/web/frontcontroller/v5/FrontControllerServletV5.java
+++ src/main/java/com/edu/springmvc1/web/frontcontroller/v5/FrontControllerServletV5.java
@@ -6,7 +6,11 @@ import com.edu.springmvc1.web.frontcontroller.MyView;
 import com.edu.springmvc1.web.frontcontroller.v3.controller.MemberFormControllerV3;
 import com.edu.springmvc1.web.frontcontroller.v3.controller.MemberListControllerV3;
 import com.edu.springmvc1.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+import com.edu.springmvc1.web.frontcontroller.v4.controller.MemberFormControllerV4;
+import com.edu.springmvc1.web.frontcontroller.v4.controller.MemberListControllerV4;
+import com.edu.springmvc1.web.frontcontroller.v4.controller.MemberSaveControllerV4;
 import com.edu.springmvc1.web.frontcontroller.v5.adapter.ControllerV3HandlerAdapter;
+import com.edu.springmvc1.web.frontcontroller.v5.adapter.ControllerV4HandlerAdapter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -34,10 +38,16 @@ public class FrontControllerServletV5 extends HttpServlet {
         handlerMappingMap.put("/front-controller/v5/v3/members/new-form", new MemberFormControllerV3());
         handlerMappingMap.put("/front-controller/v5/v3/members/save", new MemberSaveControllerV3());
         handlerMappingMap.put("/front-controller/v5/v3/members", new MemberListControllerV3());
+
+        //V4 추가
+        handlerMappingMap.put("/front-controller/v5/v4/members/new-form", new MemberFormControllerV4());
+        handlerMappingMap.put("/front-controller/v5/v4/members/save", new MemberSaveControllerV4());
+        handlerMappingMap.put("/front-controller/v5/v4/members", new MemberListControllerV4());
     }
 
     private void initHandlerAdapters() {
         handlerAdapters.add(new ControllerV3HandlerAdapter());
+        handlerAdapters.add(new ControllerV4HandlerAdapter());
     }
 
     @Override
@@ -54,10 +64,9 @@ public class FrontControllerServletV5 extends HttpServlet {
 
         ModelView mv = adapter.handle(request, response, handler);
 
-
         String viewName = mv.getViewName();//논리이름 new-form
-
         MyView view = viewResolver(viewName);
+        
         view.render(mv.getModel(), request, response);
 
     }
